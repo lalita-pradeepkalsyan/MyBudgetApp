a9f33d9abed7346caef319be664add9a58855d40
Lalita Yadav <lalita@Lalitas-MacBook-Pro.local>
2026-01-21 18:35:09 -0500

    changed amount type to decimal in model and to number in view

--- MyBudgetApp/Models/Transaction.cs
+++ MyBudgetApp/Models/Transaction.cs
@@ -9,7 +9,7 @@ namespace MyBudgetApp.Models
         public DateOnly TransactionDate { get; set; }
 
         [Required]
-        public double Amount { get; set; }
+        public decimal Amount { get; set; }
 
         [Required]
         public string? TransactionType { get; set; }
